430541cea22db28e83a0e7286d565bb60e5d00b8
Rogelio <rogemadrid@gmail.com>
2018-01-19 00:01:39 +0100

    Initial commit

---
+++ src/lu/bitcoinfund/AllIn.java
@@ -0,0 +1,11 @@
+package lu.bitcoinfund;
+
+/**
+ * Created by pc on 18.01.2018.
+ */
+public class AllIn {
+    public static void main(String[] args) {
+        System.out.println("All In!");
+        System.out.println("HODLERS !");
+    }
+}
