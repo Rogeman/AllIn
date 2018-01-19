68892b1f37fb59bde0825668b48d180af73480e4
Rogelio <rogemadrid@gmail.com>
2018-01-19 23:37:31 +0100

    testing commit

--- src/lu/bitcoinfund/AllIn.java
+++ src/lu/bitcoinfund/AllIn.java
@@ -1,10 +1,9 @@
 package lu.bitcoinfund;
 
-import java.io.UnsupportedEncodingException;
+
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
+
 
 /**
  * Created by pc on 18.01.2018.
@@ -13,7 +12,7 @@ public class AllIn {
     public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
         //example apiKey vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A
         //example secretKey NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j
-        final String secretKey = new String("NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j") ;
+        final String secretKey = new String("NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j");
         final String apiKey = new String("vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A");
 
         System.out.println("All In!");
@@ -21,48 +20,10 @@ public class AllIn {
         // openssl dgst -sha256 -hmac "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j"
 
         long unixTime = System.currentTimeMillis() / 1000L;
-        String request="symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTCquantity=1&price=0.1&recvWindow=5000&timestamp="+unixTime;
-        request="symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559";
+        String request = "symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTCquantity=1&price=0.1&recvWindow=5000&timestamp=" + unixTime;
+        request = "symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559";
         System.out.println(request);
+        String hashSignature = HashSigner.sign(secretKey, request);
 
-        Mac sha256_HMAC = null;
-        String result = null;
-        String key =  secretKey;
-
-        try{
-            byte [] byteKey = key.getBytes("UTF-8");
-            final String HMAC_SHA256 = "HmacSHA256";
-            sha256_HMAC = Mac.getInstance(HMAC_SHA256);
-            SecretKeySpec keySpec = new SecretKeySpec(byteKey, HMAC_SHA256);
-            sha256_HMAC.init(keySpec);
-            byte [] mac_data = sha256_HMAC.
-                    doFinal(request.getBytes("UTF-8"));
-            //result = Base64.encode(mac_data);
-            result = bytesToHex(mac_data);
-            System.out.println(result);
-        } catch (UnsupportedEncodingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }finally{
-            System.out.println("Done");
-        }
-
-    }
-
-    public static String bytesToHex(byte[] bytes) {
-        final  char[] hexArray = "0123456789ABCDEF".toCharArray();
-        char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
     }
 }
---
+++ src/lu/bitcoinfund/HashSigner.java
@@ -0,0 +1,59 @@
+package lu.bitcoinfund;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by pc on 19.01.2018.
+ */
+public abstract class HashSigner {
+
+
+    public static String sign(String key, String request) {
+
+        Mac sha256_HMAC = null;
+        String result = null;
+
+
+        try {
+            byte[] byteKey = key.getBytes("UTF-8");
+            final String HMAC_SHA256 = "HmacSHA256";
+            sha256_HMAC = Mac.getInstance(HMAC_SHA256);
+            SecretKeySpec keySpec = new SecretKeySpec(byteKey, HMAC_SHA256);
+            sha256_HMAC.init(keySpec);
+            byte[] mac_data = sha256_HMAC.
+                    doFinal(request.getBytes("UTF-8"));
+            //result = Base64.encode(mac_data);
+            result = bytesToHex(mac_data);
+            System.out.println(result);
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            System.out.println("Done");
+            return result;
+        }
+    }
+
+
+
+    public static String bytesToHex(byte[] bytes) {
+        final  char[] hexArray = "0123456789ABCDEF".toCharArray();
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
