20919673b3656491b13df0d2ddc8236516754ffc
Rogelio <rogemadrid@gmail.com>
2018-01-19 23:46:40 +0100

    testing commit

--- src/lu/bitcoinfund/AllIn.java
+++ src/lu/bitcoinfund/AllIn.java
@@ -6,7 +6,7 @@ import java.security.NoSuchAlgorithmException;
 
 
 /**
- * Created by pc on 18.01.2018.
+ * Created by Roge on 18.01.2018.
  */
 public class AllIn {
     public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
@@ -16,14 +16,17 @@ public class AllIn {
         final String apiKey = new String("vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A");
 
         System.out.println("All In!");
-        //[linux]$ echo -n "symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTCquantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559" |
-        // openssl dgst -sha256 -hmac "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j"
-
         long unixTime = System.currentTimeMillis() / 1000L;
         String request = "symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTCquantity=1&price=0.1&recvWindow=5000&timestamp=" + unixTime;
-        request = "symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559";
         System.out.println(request);
         String hashSignature = HashSigner.sign(secretKey, request);
+
+        /*(HMAC SHA256)
+        [linux]$ curl -H "X-MBX-APIKEY: vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A"
+        -X POST 'https://api.binance.com/api/v3/order?symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC'
+        -d 'quantity=1&price=0.1&recvWindow=6000000&timestamp=1499827319559&signature=0fd168b8ddb4876a0358a8d14d0c9f3da0e9b20c5d52b2a00fcf7d1c602f9a77'
+        */
+
 
     }
 }
--- src/lu/bitcoinfund/HashSigner.java
+++ src/lu/bitcoinfund/HashSigner.java
@@ -7,13 +7,14 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * Created by pc on 19.01.2018.
+ * Created by Roge on 19.01.2018.
  */
 public abstract class HashSigner {
 
 
     public static String sign(String key, String request) {
-
+        //This method produces the same output as the linux command:
+        //[linux]$ echo -n "symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTCquantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559" |openssl dgst -sha256 -hmac "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j"
         Mac sha256_HMAC = null;
         String result = null;
 
